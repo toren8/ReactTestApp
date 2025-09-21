d1bdd511226c8410502303ef1e6502ec2fe1aa1f
nick <paul.n.meyer@gmail.com>
2025-09-21 16:53:59 -0700

    feat:  Connect app with back-end

--- src/App.js
+++ src/App.js
@@ -1,23 +1,23 @@
 import logo from './logo.svg';
 import './App.css';
 
+import { useEffect, useState } from 'react';
+
 function App() {
+  const [message, setMessage] = useState('');
+
+  useEffect(() => {
+    fetch('http://localhost:8080/api/data?name=React')
+      .then(response => response.text())
+      .then(data => setMessage(data))
+      .catch(error => console.error('Error fetching data:', error));
+  }, []);
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      Some stuff
+      <h1>{message}</h1>
+      More Stuff
     </div>
   );
 }
